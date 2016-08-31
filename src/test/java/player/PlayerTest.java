51d5c96c9886e060b1842a031a794eba25ee5369
ggattipinheiro <ggattipinheiro@mediametrie.fr>
2016-08-31 13:16:50 +0200

    Player test - TODO

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -1,11 +1,16 @@
 package player;
 
 import org.assertj.core.api.WithAssertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import de.bechte.junit.runners.context.HierarchicalContextRunner;
+@DisplayName("A player")
+class PlayerTest implements WithAssertions {
 
-@RunWith(HierarchicalContextRunner.class)
-public class PlayerTest implements WithAssertions {
+    @Test
+    @DisplayName("tmp")
+    void tmp() {
+
+    }
 
 }
